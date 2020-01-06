0c05da5dd1294e2094ca54a62e10a71e617c7a6e
marijoblaz <46202325+marijoblaz@users.noreply.github.com>
2020-01-06 23:26:29 +0100

    Update and rename iOWatch.ino to iOwatch_v01.ino
    Added support for an extra LED and optimized the buttons

--- iOwatch_v01.ino
+++ iOwatch_v01.ino
@@ -27,13 +27,15 @@ const byte leds[24] = {10, A0, A1, A2, A3, 13, 4, 5, 6, 7, 8, 9};  // 12 LED's a
 static byte buttonPin = 2;                           // assigning button pin value
 static byte buttonPin2 = 3;                           // assigning button pin value
 
+static byte userLed = 11;
+
 boolean timeSettingMode = false;               // time setting mode
 boolean timeSettingModeHours = false;          // time setting mode hours [0, 11]
 boolean timeSettingModeMinutes = false;        // time setting mode minutes [0, 5, 10, .., 55]
 boolean timeSettingModeMinutesToFive = false;  // time setting mode minutes [0-4]
 
-OneButton button(2, true);      // button
-OneButton button2(3, true);      // button
+OneButton button(3, true);      // button
+OneButton button2(2, true);      // button
 
 void setup () {
   Serial.begin(9600);         //enabling serial communication at 9600 boud rate
@@ -50,14 +52,19 @@ void setup () {
 
   delay(40);
 
+  pinMode(userLed, OUTPUT);             // assigning all pins in array as output
+
   attachInterrupt(0, wakeFunction, FALLING);        //interrupt for waking up
   attachInterrupt(1, wakeFunction, FALLING);        //interrupt for waking up
-  button.attachDoubleClick(doubleclick);            // link the function to be called on a doubleclick event.
+  
+  //button.attachDoubleClick(doubleclick);            // link the function to be called on a doubleclick event.
   button.attachClick(singleclick);                  // link the function to be called on a singleclick event.
   button.attachLongPressStop(longclick);            // link the function to be called on a longpress event.
-  button2.attachDoubleClick(doubleclick);            // link the function to be called on a doubleclick event.
-  button2.attachClick(singleclick);                  // link the function to be called on a singleclick event.
-  button2.attachLongPressStop(longclick);            // link the function to be called on a longpress event.
+  
+  //button2.attachDoubleClick(doubleclick);            // link the function to be called on a doubleclick event.
+  button2.attachClick(singleclick2);                  // link the function to be called on a singleclick event.
+  //button2.attachLongPressStop(longclick);            // link the function to be called on a longpress event.
+  
   shutdownNow();      //calling deep sleep function
 }
 
@@ -65,8 +72,8 @@ void loop (){
   
    button.tick();     //checking if button is pressed and long
    delay(10);         //needed for stable operation and for button ticking
-   button2.tick();     //checking if button is pressed and long
-   
+   button2.tick();     //checking if button 2 is pressed and long
+   Serial.println("Tick");
    
 }
 
@@ -75,6 +82,7 @@ void wakeFunction(){  //interrupt function when button is presse
 }
 
 void singleclick(){   //single Click button
+  
  if(!timeSettingMode){
 
     unsigned int DelayHoursLedON = 500;           // how long will hour LED light up after getting time (in microseconds)
@@ -117,6 +125,72 @@ void singleclick(){   //single Click button
  }else{
   if(timeSettingModeHours){         //hours scrolling trough LED's
     digitalWrite(leds[settingHours], LOW);
+    if(settingHours == 0){
+      settingHours = 11;
+      digitalWrite(leds[settingHours], HIGH);
+    }else{
+      settingHours--;
+      digitalWrite(leds[settingHours], HIGH);
+    }
+  }
+  if(timeSettingModeMinutes){       //minutes scrolling trough LED's
+    digitalWrite(leds[settingMinutes], LOW);
+    if(settingMinutes == 0){
+      settingMinutes = 11;
+      digitalWrite(leds[settingMinutes], HIGH);
+    }else{
+      settingMinutes--;
+      digitalWrite(leds[settingMinutes], HIGH);
+    }
+  }
+  if(timeSettingModeMinutesToFive){       //minutes to five scrolling trough LED's
+    digitalWrite(leds[settingMinutesToFive], LOW);
+    if(settingMinutesToFive == 0){
+      settingMinutesToFive = 4;
+      digitalWrite(leds[settingMinutesToFive], HIGH);
+    }else{
+      settingMinutesToFive--;
+      digitalWrite(leds[settingMinutesToFive], HIGH);
+    }
+  }
+ }
+}
+
+void singleclick2(){   //single Click button
+ if(!timeSettingMode){
+
+    allOff();         //turning all LED's off
+
+    digitalWrite(userLed, HIGH);
+    
+    unsigned int DelayTemperatureLedON = 150;     // how long will minutes LED light up after getting time in microseconds
+    unsigned int DelayTemperatureLedOFF = 280;    // how long will minutes LED stay off after showing time in microseconds
+      
+    temperatures = rtc.getTemperature();
+    temperaturesOnFive = temperatures / 5;
+    for (int i=0; i <= temperaturesOnFive ; i++){ 
+      digitalWrite(leds[i], HIGH);
+      delay(500);
+    } 
+    for (int i=0; i <= temperaturesOnFive ; i++){  
+     digitalWrite(leds[i], LOW);
+     delay(200);
+    }
+    delay(500);
+    temperatures = temperatures - (temperaturesOnFive * 5);
+    for (int i=1; i <= temperatures ; i++){
+      digitalWrite(leds[i], HIGH);
+      delay(500);
+    }
+    for (int i=1; i <= temperatures ; i++){       
+     digitalWrite(leds[i], LOW);
+     delay(200);
+    }
+    
+     shutdownNow();
+ }else{
+  if(timeSettingModeHours){         //hours scrolling trough LED's
+    digitalWrite(leds[settingHours], LOW);
     if(settingHours == 11){
       settingHours = 0;
       digitalWrite(leds[settingHours], HIGH);
@@ -149,10 +223,11 @@ void singleclick(){   //single Click button
 }
 
 void longclick(){         //longclick function for time setting
+  
   if(timeSettingModeMinutesToFive && !timeSettingModeMinutes){
     timeSettingModeMinutes = true;  // pass
   }
-
+ 
   if(timeSettingMode){
     if(timeSettingModeMinutes){
       if(timeSettingModeMinutesToFive){
@@ -193,73 +268,23 @@ void longclick(){         //longclick function for time setting
   }
 }
 
-void doubleclick(){           //function displaying the temperature
-  if(!timeSettingMode){
-
-    allOff();         //turning all LED's off
-    
-    unsigned int DelayTemperatureLedON = 150;     // how long will minutes LED light up after getting time in microseconds
-    unsigned int DelayTemperatureLedOFF = 280;    // how long will minutes LED stay off after showing time in microseconds
-      
-    temperatures = rtc.getTemperature();
-    temperaturesOnFive = temperatures / 5;
-    for (int i=0; i <= temperaturesOnFive ; i++){ 
-      digitalWrite(leds[i], HIGH);
-      delay(500);
-    } 
-    for (int i=0; i <= temperaturesOnFive ; i++){  
-     digitalWrite(leds[i], LOW);
-     delay(200);
-    }
-    delay(500);
-    temperatures = temperatures - (temperaturesOnFive * 5);
-    for (int i=1; i <= temperatures ; i++){
-      digitalWrite(leds[i], HIGH);
-      delay(500);
-    }
-    for (int i=1; i <= temperatures ; i++){       
-     digitalWrite(leds[i], LOW);
-     delay(200);
-    }
-    
-     shutdownNow();
-  }
-}
-
 void allOff(){
   for (int i=0; i<=12; i++){
     digitalWrite(leds[i], LOW);       //turning all LED's off
-  }  
+  }
+  digitalWrite(userLed, LOW);
 }
 
 void allOn(){         //function turning all LED's on in specific pattern
-  digitalWrite(leds[3], HIGH);
-  digitalWrite(leds[6], HIGH);
-  digitalWrite(leds[9], HIGH);
-  digitalWrite(leds[0], HIGH);
-  delay(300);
-  for (int i=1; i<=2; i++){
-    digitalWrite(leds[i], HIGH);
-    delay(150);
-  }
-  for(int i=11; i>=10; i--){
-    digitalWrite(leds[i], HIGH);
-    delay(150);  
-  }
-  for(int i=5; i>=4; i--){
-    digitalWrite(leds[i], HIGH);
-    delay(150);  
-  }
-  for (int i=7; i<=8; i++){
-    digitalWrite(leds[i], HIGH);
-    delay(150);
-  }
+   for (int i=0; i<=12; i++){
+    digitalWrite(leds[i], HIGH);       //turning all LED's off
+    delay(100);
+  }  
 }
 
 void shutdownNow(){
     allOff();
-//  Serial.println("going to sleep");
+    Serial.println("going to sleep");
     delay(500);
     LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); //deep sleep
 }
-
